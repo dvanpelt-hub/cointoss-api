4ed9945e42d22f1dcf0bb25003c7fdd75a88f8c3
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-03-30 17:42:24 +0100

    Updated migration to adjust ticker_symbol length

--- migrations/001.do.create_coin_details_table.sql
+++ migrations/001.do.create_coin_details_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE coin_details (
   id BIGSERIAL NOT NULL PRIMARY KEY,
-  ticker_symbol VARCHAR(10) NOT NULL UNIQUE,
+  ticker_symbol VARCHAR(30) NOT NULL UNIQUE,
   up_votes NUMERIC NOT NULL,
   down_votes NUMERIC NOT NULL
 );
