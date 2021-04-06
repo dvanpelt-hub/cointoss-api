6d6c3fc6a88f4815d85e0d1063d81daa031816c8
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-04-06 15:49:02 +0100

    Updated Cors configuration

--- cors/cors.js
+++ cors/cors.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const CLIENT_ORIGIN = {
   origin: [
     "http://localhost:8000",
-    // "https://pocket-stocks-client.vercel.app/home",
-    // "https://quiet-woodland-22837.herokuapp.com",
+    "https://calm-taiga-71106.herokuapp.com/api/v1/coins",
+    "https://cointoss-client-dvanpelt-hub.vercel.app",
   ],
   optionsSuccessStatus: 200,
 };
