c083a0a9ecf7e162e48b69e96e8d0ac41235add3
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-04-06 15:54:47 +0100

    Updated CORS policy

--- cors/cors.js
+++ cors/cors.js
@@ -5,6 +5,7 @@ const CLIENT_ORIGIN = {
     "http://localhost:8000",
     "https://calm-taiga-71106.herokuapp.com/api/v1/coins",
     "https://cointoss-client-dvanpelt-hub.vercel.app",
+    "https://cointoss-client.vercel.app/coins",
   ],
   optionsSuccessStatus: 200,
 };
